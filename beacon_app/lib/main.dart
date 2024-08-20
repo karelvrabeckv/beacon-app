cc0aa3362b2c891ed120556b86ca7c19382bf2b3
Karel Vrabec <karelvrabeckv@gmail.com>
2024-08-20 16:59:21 +0200

    Scanning (and detecting beacons!) application DONE

--- beacon_app/lib/main.dart
+++ beacon_app/lib/main.dart
@@ -3,7 +3,8 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_beacon/flutter_beacon.dart' as fb;
+import 'package:flutter_beacon/flutter_beacon.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() {
   runApp(const BeaconScanner());
@@ -20,24 +21,21 @@ class BeaconScanner extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
-      home: const BeaconScannerPage(title: 'Beacon Scanner'),
+      home: const BeaconScannerPage(),
     );
   }
 }
 
 class BeaconScannerPage extends StatefulWidget {
-  const BeaconScannerPage({super.key, required this.title});
-
-  final String title;
+  const BeaconScannerPage({super.key});
 
   @override
   State<BeaconScannerPage> createState() => _BeaconScannerPageState();
 }
 
 class _BeaconScannerPageState extends State<BeaconScannerPage> {
-  StreamSubscription? _beaconStream;
-  List<fb.Beacon> beacons = [];
-  List<fb.Region> regions = [];
+  var targetedBeacon;
+  List<Region> regions = [];
 
   @override
   void initState() {
@@ -47,35 +45,37 @@ class _BeaconScannerPageState extends State<BeaconScannerPage> {
     scanning
       .then((value) {
         if (kDebugMode) {
-          print('----------------------------------------');
-          print('\nSCANNING SUCCESSFULLY INITIATED!!!\n');
-          print('----------------------------------------');
+          print('\x1B[32m''SCANNING SUCCESSFULLY INITIATED''\x1B[32m');
         }
       })
       .catchError((error) {
         if (kDebugMode) {
-          print('----------------------------------------');
-          print('\nERROR OCCURED WHILE SCANNING!!!\n');
-          print('----------------------------------------');
+          print('\x1B[31m''ERROR OCCURED WHILE SCANNING''\x1B[31m');
         }
       });
   }
 
-  void _addBeacon(fb.Beacon beacon) {
-    beacons.add(beacon);
+  void _addBeacon(Beacon beacon) {
+    setState(() {
+      targetedBeacon = beacon;
+    });
   }
 
   Future<void> initBeaconScanning() async {
     try {
-      await fb.flutterBeacon.initializeScanning;
+      await Permission.location.request();
+      await Permission.bluetoothScan.request();
+      await Permission.bluetoothConnect.request();
+
+      await flutterBeacon.initializeScanning;
     } on PlatformException catch (e) {
       if (kDebugMode) {
-        print(e);
+        print('\x1B[31m''$e''\x1B[31m');
       }
       return;
     }
 
-    var region = fb.Region(
+    var region = Region(
       identifier: 'IOTS_TG_DC8E5D2',
       proximityUUID: 'ffffffff-1070-1234-5678-123456789123',
       major: 1000,
@@ -83,30 +83,30 @@ class _BeaconScannerPageState extends State<BeaconScannerPage> {
     );
 
     regions.add(region);
-  
-    if (kDebugMode) {
-      print('----------------------------------------');
-      print(regions);
-      print('----------------------------------------');
-    }
 
     if (regions.isNotEmpty) {
-      _beaconStream = fb.flutterBeacon.ranging(regions).listen((result) {
+      flutterBeacon.ranging(regions).listen((result) {
         if (result.beacons.isNotEmpty) {
-          if (kDebugMode) {
-            print('----------------------------------------');
-            print('BEACONS FOUND!!!');
-            print('----------------------------------------');
-          }
-
           for (var beacon in result.beacons) {
             _addBeacon(beacon);
           }
+
+          if (kDebugMode) {
+            print('\x1B[32m''BEACON FOUND''\x1B[0m');
+            print(''
+              'proximityUUID: ${targetedBeacon.proximityUUID}, '
+              'macAddress: ${targetedBeacon.macAddress}, '
+              'major: ${targetedBeacon.major}, '
+              'minor: ${targetedBeacon.minor}, '
+              'rssi: ${targetedBeacon.rssi}, '
+              'txPower: ${targetedBeacon.txPower}, '
+              'accuracy: ${targetedBeacon.accuracy}, '
+              'proximity: ${targetedBeacon.proximity}'
+            '');
+          }
         } else {
             if (kDebugMode) {
-              print('----------------------------------------');
-              print('NOTHING FOUND SO FAR!!!');
-              print('----------------------------------------');
+              print('\x1B[31m''BEACONS NOT FOUND''\x1B[31m');
             }
         }
       });
@@ -118,26 +118,37 @@ class _BeaconScannerPageState extends State<BeaconScannerPage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Beacons'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'Some text',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('proximityUUID', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.proximityUUID}'),
+            const SizedBox(height: 12),
+            Text('macAddress', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.macAddress}'),
+            const SizedBox(height: 12),
+            Text('major', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.major}'),
+            const SizedBox(height: 12),
+            Text('minor', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.minor}'),
+            const SizedBox(height: 12),
+            Text('rssi', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.rssi}'),
+            const SizedBox(height: 12),
+            Text('txPower', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.txPower}'),
+            const SizedBox(height: 12),
+            Text('accuracy', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.accuracy}'),
+            const SizedBox(height: 12),
+            Text('proximity', style: Theme.of(context).textTheme.headlineMedium),
+            Text('${targetedBeacon?.proximity}'),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
