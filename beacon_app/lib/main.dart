02fbc319669f6c8cee6203f44c7ef68d38976088
Karel Vrabec <karelvrabeckv@gmail.com>
2024-08-20 14:54:46 +0200

    Scanning (but not detecting beacons) application DONE

--- beacon_app/lib/main.dart
+++ beacon_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_beacon/flutter_beacon.dart' as lib;
-import 'package:beacon_app/beacon.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_beacon/flutter_beacon.dart' as fb;
 
 void main() {
   runApp(const BeaconScanner());
@@ -34,13 +35,82 @@ class BeaconScannerPage extends StatefulWidget {
 }
 
 class _BeaconScannerPageState extends State<BeaconScannerPage> {
-  StreamSubscription? beaconStream;
-  List<Beacon> beacons = [];
+  StreamSubscription? _beaconStream;
+  List<fb.Beacon> beacons = [];
+  List<fb.Region> regions = [];
 
-  void _addBeacon(Beacon beacon) {
-    setState(() {
-      beacons = [...beacons, beacon]; 
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    Future<void> scanning = initBeaconScanning();
+    scanning
+      .then((value) {
+        if (kDebugMode) {
+          print('----------------------------------------');
+          print('\nSCANNING SUCCESSFULLY INITIATED!!!\n');
+          print('----------------------------------------');
+        }
+      })
+      .catchError((error) {
+        if (kDebugMode) {
+          print('----------------------------------------');
+          print('\nERROR OCCURED WHILE SCANNING!!!\n');
+          print('----------------------------------------');
+        }
+      });
+  }
+
+  void _addBeacon(fb.Beacon beacon) {
+    beacons.add(beacon);
+  }
+
+  Future<void> initBeaconScanning() async {
+    try {
+      await fb.flutterBeacon.initializeScanning;
+    } on PlatformException catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return;
+    }
+
+    var region = fb.Region(
+      identifier: 'IOTS_TG_DC8E5D2',
+      proximityUUID: 'ffffffff-1070-1234-5678-123456789123',
+      major: 1000,
+      minor: 1138,
+    );
+
+    regions.add(region);
+  
+    if (kDebugMode) {
+      print('----------------------------------------');
+      print(regions);
+      print('----------------------------------------');
+    }
+
+    if (regions.isNotEmpty) {
+      _beaconStream = fb.flutterBeacon.ranging(regions).listen((result) {
+        if (result.beacons.isNotEmpty) {
+          if (kDebugMode) {
+            print('----------------------------------------');
+            print('BEACONS FOUND!!!');
+            print('----------------------------------------');
+          }
+
+          for (var beacon in result.beacons) {
+            _addBeacon(beacon);
+          }
+        } else {
+            if (kDebugMode) {
+              print('----------------------------------------');
+              print('NOTHING FOUND SO FAR!!!');
+              print('----------------------------------------');
+            }
+        }
+      });
+    }
   }
 
   @override
